c5528a4fbc1cc176313c829d6e0354e11372cafe
Bethuel Khisa <sunguribethuel@gmail.com>
2024-06-29 05:02:49 +0300

    Adds functions to index.js file

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+var bestCustomer;
+
+const leastFavoriteCustomer = "Ruto";
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  return (window.bestCustomer = "not bob");
+}
+
+function overwriteBestCustomer() {
+  let bestCustomer = "maybe bob";
+
+  return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  let leastFavoriteCustomer = "Rigathi";
+
+  return (leastFavoriteCustomer = "Rigathi");
+}
